d557ed376a57ce2637555db485be9d187ca15f19
anushkapolleyVE <anushkapolley@virtualemployee.com>
2026-07-02 09:27:01 +0530

    Add backend files

--- backend/app/database/database.py
+++ backend/app/database/database.py
@@ -3,7 +3,10 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 from app.core.config import settings
 
-engine = create_engine(settings.DATABASE_URL)
+engine = create_engine(
+    settings.DATABASE_URL,
+    connect_args={"sslmode": "require"},
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
@@ -12,11 +15,3 @@ SessionLocal = sessionmaker(
 )
 
 Base = declarative_base()
-
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
